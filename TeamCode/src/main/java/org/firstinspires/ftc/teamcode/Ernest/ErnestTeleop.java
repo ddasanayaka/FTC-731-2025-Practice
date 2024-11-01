0567a0fc817d3e62d82afc28d353a4143452623d
ernest <ehu1@guilford.edu>
2024-11-01 15:20:12 -0700

    ernest telop commit 11/1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ernest/ErnestTeleop.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.Ernest;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp()
+public class ErnestTeleop extends OpMode {
+    @Override
+    public void init() {
+        telemetry.adddata("red", "blue");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+}
