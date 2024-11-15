f8afa23eee69d188f2454ce0f2486a4815ee5a8c
AlexWang <alexandershinnwang@gmail.com>
2024-11-15 17:07:14 -0500

    First Attempt Practice 6

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Alex_Wang/TeleOpGamepad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Alex_Wang/TeleOpGamepad.java
@@ -3,15 +3,18 @@ package org.firstinspires.ftc.teamcode.Alex_Wang;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.Ernest.GamepadButton;
+
 @TeleOp()
 public class TeleOpGamepad extends OpMode {
 @Override
     public void init() {
 }
 
+int a = 1;
 @Override
     public void loop() {
-telemetry.addData("A button", gamepad1.a);
-}
+    telemetry.addData(String.valueOf(a + 1), gamepad1.a);
+    telemetry.addData(String.valueOf(a - 1), gamepad1.b);
+} }
 
-}
