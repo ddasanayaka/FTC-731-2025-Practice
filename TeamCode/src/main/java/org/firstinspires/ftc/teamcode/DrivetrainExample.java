7ea5da07dc8bae05a0f814aa4ea9f10bc600aebc
Dilan Dasanayaka <ddasanayaka02@gmail.com>
2024-11-22 17:31:02 -0500

    11/22 commit 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivetrainExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivetrainExample.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Shrimaan;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
