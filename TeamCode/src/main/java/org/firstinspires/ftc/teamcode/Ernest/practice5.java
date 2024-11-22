f897aa54d93f53580ed8b5ae17c569b8636d8886
ernest <ehu1@guilford.edu>
2024-11-22 15:30:35 -0800

    11/22 commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ernest/practice5.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ernest/practice5.java
@@ -2,9 +2,11 @@ package org.firstinspires.ftc.teamcode.Ernest;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Disabled
+@TeleOp
 public class practice5 extends OpMode {
 
     DcMotor motor;
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ernest/practice7.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.Ernest;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(group = "Ernest")
+
+public class practice7 extends OpMode {
+
+    @Override
+    public void init() {
+    }
+
+    long a = 0;
+    @Override
+    public void loop() {
+        if (gamepad1.a) {
+            a = a + 1_000_000_000;
+            telemetry.addData("A is", a);
+        }
+        if (gamepad1.b) {
+            a = a - 1_000_000_000;
+            telemetry.addData("A is", a);
+        }
+    }
+}
