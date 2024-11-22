302034a45510e5d40b27086e150750d92dd7ccaf
ShrimaanRAP <srapuru@guilford.edu>
2024-11-22 17:41:47 -0500

    Motorlearning11/222

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shrimaan/Practice2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shrimaan/Practice2.java
@@ -7,14 +7,15 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous
 public class Practice2 extends OpMode {
-
+    DcMotor motor;
     @Override
     public void init() {
-        private DcMotor ;
+        motor = hardwareMap.get(DcMotor.class , "motor");
     }
 
     @Override
     public void loop() {
 
+        motor.setPower(1);
     }
 }
