f9fa5c175460cda2e7ad5e3236a5402834d6a0e9
AlexWang <alexandershinnwang@gmail.com>
2024-11-22 17:47:25 -0500

    Basic Final Practice attempt 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Alex_Wang/BasicPracticeFinal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Alex_Wang/BasicPracticeFinal.java
@@ -6,46 +6,46 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 // TODO: There is 1 mistake in the code, find it, correct it, and explain what difference it makes. Hints are at the bottom
-// Line number of mistake:
-// Difference:
+// Line number of mistake: 74
+// Difference: double strafe = -gamepad1.left_stick_y;
 
 // Assume that the control hub use is connected to 4 motors which don't have encoders, and there is no other hardware connected
 // Assume that moving the left stick up moves the bot forward, right moves the bot right, and moving the right stick right makes the bot rotate right
 // The above controls are standard for FTC
 
 // TODO: What must the names of the motors on the configuration file be?
-// Answer:
+// Answer: leftFront, leftBack, rightFront, rightBack
 
 // TODO: Will this OpMode show up on the Driver Hub?
-// Answer:
+// Answer: No, because it is disabled.
 @Disabled
 @TeleOp
 public final class BasicPracticeFinal extends OpMode {
     // TODO: What is happening in the following 2 lines?
-    // Answer:
+    // Answer: The motors are being created and a double called "runtime" is also being created.
     private DcMotor leftFrontMotor, leftBackMotor, rightFrontMotor, rightBackMotor;
     private double runtime;
 
     @Override
     public void init() {
         // TODO: What is happening in the following 4 lines?
-        // Answer:
+        // Answer: The four motors are being taken from the hardware map and are being configured
         leftFrontMotor = hardwareMap.get(DcMotor.class,"leftFront");
         leftBackMotor = hardwareMap.get(DcMotor.class,"leftBack");
         rightFrontMotor = hardwareMap.get(DcMotor.class,"rightFront");
         rightBackMotor = hardwareMap.get(DcMotor.class,"rightBack");
 
         // TODO: What is happening in the following 4 lines and why are they needed?
-        // Answer:
+        // Answer: The motors' direction are being set to spin a specific way
         leftFrontMotor.setDirection(DcMotor.Direction.REVERSE);
         leftBackMotor.setDirection(DcMotor.Direction.REVERSE);
         rightFrontMotor.setDirection(DcMotor.Direction.FORWARD);
         rightBackMotor.setDirection(DcMotor.Direction.FORWARD);
         // TODO: Which of the previous 4 lines are unnecessary
-        // Answer:
+        // Answer: The two right motors, as the default spin is set to forward
 
         // TODO: Should the motors follow RunMode "RUN_USING_ENCODER"?
-        // Answer:
+        // Answer: Yes as running without encoder may lead to the bot eventually straying off or being uncontrolled
         leftFrontMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         leftBackMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightFrontMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -55,7 +55,7 @@ public final class BasicPracticeFinal extends OpMode {
     @Override
     public void init_loop() {
         // TODO: What do the following lines do?
-        // Answer:
+        // Answer: Prints out a specific value "Time in INIT" and then the runtime
         runtime = getRuntime();
         telemetry.addData("Time in INIT",runtime);
     }
@@ -63,14 +63,14 @@ public final class BasicPracticeFinal extends OpMode {
     @Override
     public void start() {
         // TODO: What does the following line do?
-        // Answer:
+        // Answer: Once it starts, the run time and, in extension, the timer are reset
         resetRuntime();
     }
 
     @Override
     public void loop() {
         // TODO: What is happening in the following 3 lines?
-        // Answer:
+        // Answer: Setting multiple double values to specific inputs on the gamepad (left stick y, left stick x, right stick x)
         double forward = gamepad1.left_stick_y;
         double strafe = gamepad1.left_stick_x;
         double rotate = gamepad1.right_stick_x;
@@ -84,7 +84,7 @@ public final class BasicPracticeFinal extends OpMode {
 
         // Since the wheel powers are comprised of multiple inputs, the power set to the motors must be normalized (lines 81-90)
         // TODO: What range of inputs must the motor power be set to?
-        // Answer:
+        // Answer: From -1 to 1 (-1 and 1 are highest power in opposite spinning directions)
         double maxSpeed = 1.0;
         maxSpeed = Math.max(maxSpeed, Math.abs(frontLeftPower));
         maxSpeed = Math.max(maxSpeed, Math.abs(frontRightPower));
@@ -97,19 +97,20 @@ public final class BasicPracticeFinal extends OpMode {
         backRightPower /= maxSpeed;
 
         // TODO: What is happening in the following 4 lines?
-        // Answer:
+        // Answer: The motors are getting their powers set to specific double values
         leftFrontMotor.setPower(frontLeftPower);
         leftBackMotor.setPower(frontRightPower);
         rightFrontMotor.setPower(backLeftPower);
         rightBackMotor.setPower(backRightPower);
 
         // TODO: What is happening in the following 3 lines?
-        // Answer:
+        // Answer: Printing out captions to help with the process, giving the forward, strafe, and rotate values
         telemetry.addData("Forward",forward);
         telemetry.addData("Strafe",strafe);
         telemetry.addData("Rotate",rotate);
 
         // TODO: What is happening in the following 2 lines?
+        // Answer: Taking the double run time and printing it out as a timer of sorts, giving the "Time" then the runtime
         runtime = getRuntime();
         telemetry.addData("Time",runtime);
     }
